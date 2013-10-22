d236bada3d94bd60528bd13f0aae4e819f0a99c9
Sebastian Ruiz <sebastian.ruiz@me.com>
2013-10-22 19:29:13 +0100

    first commit

---
+++ dist/js/libs/main.js
@@ -0,0 +1,100 @@
+(function (c) {
+    c.fn.noisy = function (b) {
+        var b = c.extend({}, c.fn.noisy.defaults, b),
+            d, h, a = !1;
+        try {
+            h = "localStorage" in window && window.localStorage !== null
+        } catch (l) {
+            h = !1
+        }
+        window.JSON && h && (a = localStorage.getItem(window.JSON.stringify(b)));
+        if (a) d = a;
+        else {
+            a = document.createElement("canvas");
+            if (a.getContext) {
+                a.width = a.height = b.size;
+                for (var i = a.getContext("2d"), e = i.createImageData(a.width, a.height), j = b.intensity * Math.pow(b.size, 2), k = 255 * b.opacity; j--;) {
+                    var f = ~~ (Math.random() * a.width),
+                        g = ~~ (Math.random() * a.height),
+                        f = (f +
+                            g * e.width) * 4,
+                        g = j % 255;
+                    e.data[f] = g;
+                    e.data[f + 1] = b.monochrome ? g : ~~(Math.random() * 255);
+                    e.data[f + 2] = b.monochrome ? g : ~~(Math.random() * 255);
+                    e.data[f + 3] = ~~ (Math.random() * k)
+                }
+                i.putImageData(e, 0, 0);
+                d = a.toDataURL("image/png");
+                //if (d.indexOf("data:image/png") != 0 || c.browser.msie && c.browser.version.substr(0, 1) < 9 && d.length > 32768) d = b.fallback
+            } else d = b.fallback;
+            window.JSON && h && localStorage.setItem(window.JSON.stringify(b), d)
+        }
+        return this.each(function () {
+            c(this).css("background-image", "url('" + d + "')," + c(this).css("background-image"))
+        })
+    };
+    c.fn.noisy.defaults = {
+        intensity: 0.9,
+        size: 200,
+        opacity: 0.08,
+        fallback: "",
+        monochrome: !1
+    }
+})(jQuery);
+
+$(document).ready(function () {
+
+    var myLayout = $('body').layout({
+
+        //	reference only - these options are NOT required because 'true' is the default
+        closable:					true	// pane can open & close
+        ,   applyDefaultStyles: false
+        ,	resizable:					true	// when open, pane can be resized
+        ,	slidable:					true	// when closed, pane can 'slide' open over other panes - closes on mouse-out
+        ,	livePaneResizing:			true
+
+        //	some resizing/toggling settings
+        ,	north__slidable:			false	// OVERRIDE the pane-default of 'slidable=true'
+        ,	north__togglerLength_closed: '100%'	// toggle-button is full-width of resizer-bar
+        ,	north__spacing_closed:		20		// big resizer-bar when open (zero height)
+        ,	south__resizable:			false	// OVERRIDE the pane-default of 'resizable=true'
+        ,	south__spacing_open:		0		// no resizer-bar when open (zero height)
+        ,	south__spacing_closed:		20		// big resizer-bar when open (zero height)
+
+        //	some pane-size settings
+        ,	west__minSize:				100
+        ,	east__size:					300
+        ,	east__minSize:				200
+        ,	east__maxSize:				.5 // 50% of layout width
+        ,	center__minWidth:			100
+
+        //	some pane animation settings
+        ,	west__animatePaneSizing:	false
+        ,	west__fxSpeed_size:			"fast"	// 'fast' animation when resizing west-pane
+        ,	west__fxSpeed_open:			1000	// 1-second animation when opening west-pane
+        ,	west__fxSettings_open:		{ easing: "easeOutBounce" } // 'bounce' effect when opening
+        ,	west__fxName_close:			"none"	// NO animation when closing west-pane
+
+        //	enable showOverflow on west-pane so CSS popups will overlap north pane
+        ,	west__showOverflowOnHover:	true
+
+        //	enable state management
+        ,	stateManagement__enabled:	true // automatic cookie load & save enabled by default
+
+        ,	showDebugMessages:			true // log and/or display messages from debugging & testing code
+    });
+    $('.ui-layout-center').noisy({
+        intensity: 0.9,
+        size: 200,
+        opacity: 0.05,
+        fallback: 'fallback.png',
+        monochrome: false
+    });
+    var toggle = $('.dropdown-toggle');
+    toggle.on('contextmenu',function(e){
+        e.preventDefault();
+        toggle.dropdown('toggle');
+    });
+
+});
